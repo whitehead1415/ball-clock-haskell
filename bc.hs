059aa1311485bdb9ba9caadd4c014736e1778777
Michael Whitehead <whitehead1415@gmail.com>
2014-05-17 12:38:20 -0600

    Second try

--- bc.hs
+++ bc.hs
@@ -6,16 +6,21 @@ isSeq (x:y:zs) | y == succ x = isSeq $ y:zs
 isSeq _ = False
 
 tick :: [Integer] -> [Integer] -> [Integer] -> [Integer] -> Integer -> Integer
-tick q@(ball:balls) ones fives hours m
-	| isSeq q && m > 5 = quot m 1440
-	| m `mod` 720 == 0 = tick (balls ++ ones ++ fives ++ hours ++ [ball]) [] [] [] (m + 1) 
-	| m `mod` 60 == 0 = tick (balls ++ ones ++ fives) [] [] (ball:hours) (m + 1)
-	|	m `mod` 5 == 0 = tick (balls ++ ones) [] (ball:fives) hours (m + 1)
-	| otherwise = tick balls (ball:ones) fives hours (m + 1)
+tick q@(ball:balls) ones fives hours m =
+	if m `mod` 5 == 0
+		then if m `mod` 60 == 0
+			then if m `mod` 720 == 0 
+				then
+					if isSeq (balls ++ ones ++ fives ++ hours ++ [ball]) 
+						then quot m 1440
+					else tick (balls ++ ones ++ fives ++ hours ++ [ball]) [] [] [] (m + 1) 
+			else tick (balls ++ ones ++ fives) [] [] (ball:hours) (m + 1)
+		else tick (balls ++ ones) [] (ball:fives) hours (m + 1)
+	else tick balls (ball:ones) fives hours (m + 1)
+		
 
 getDays :: Integer -> Integer
-getDays n = let q = [1..n] in
-						tick q [] [] [] 1
+getDays n = tick [1..n] [] [] [] 1
 
 main = do
 	print $ getDays 123
