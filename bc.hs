963d1479d7cd2cc30154b4b71688dfba9f767a3d
Michael Whitehead <whitehead1415@gmail.com>
2014-05-19 12:54:20 -0600

    Applies inversion of control

--- bc.hs
+++ bc.hs
@@ -6,20 +6,19 @@ isSeq (x:y:zs) | y == succ x = isSeq $ y:zs
 isSeq _ = False
 
 tickHours :: [Int] -> [Int] -> Int -> [Int]
-tickHours (ball:balls) hours 12 = balls ++ hours ++ [ball]
+tickHours q hours 12 = (\(b:bs) -> bs ++ hours ++ [b]) $ tickFives q [] 1
 tickHours q hours count = (\(b:bs) -> tickHours bs (b:hours) (count + 1)) $ tickFives q [] 1
 
 tickFives :: [Int] -> [Int] -> Int -> [Int]
-tickFives q fives 12 = q ++ fives
+tickFives q fives 12 = tickOnes q [] 1 ++ fives
 tickFives q fives count = (\(b:bs) -> tickFives bs (b:fives) (count + 1)) $ tickOnes q [] 1
 
 tickOnes :: [Int] -> [Int] -> Int -> [Int]
 tickOnes q ones 5 = q ++ ones
 tickOnes (ball:balls) ones count = tickOnes balls (ball:ones) (count + 1)
 		
-getDays :: Int -> [Int] -> [Int]
-getDays 0 q = tickHours q [] 1
---getDays halfDays q = if isSeq q then quot halfDays 2
-										 --else getDays 0 $ tickHours q [] 1
+getDays :: Int -> [Int] -> Int
+getDays halfDays q = if halfDays > 1 && isSeq q then quot halfDays 2
+										 else getDays (halfDays + 1) $ tickHours q [] 1
 
-main = print $ getDays 0 [1..30]
+main = print $ getDays 1 [1..30]
