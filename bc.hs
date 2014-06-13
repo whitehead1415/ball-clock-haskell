7b76222870adec02635d5d630039d7cbd8806a21
Michael Whitehead <whitehead1415@gmail.com>
2014-06-12 20:59:47 -0600

    Fastest yet

--- bc.hs
+++ bc.hs
@@ -1,33 +1,34 @@
-data Hold = Hold { q :: [Int], row :: [Int] } deriving (Show)
+import Data.Sequence
 
-isReset :: [Int] -> Int -> Bool
-isReset []     _ = True
-isReset (x:xs) i
+data Hold = Hold { q :: Seq Int, row :: Seq Int} deriving (Show)
+
+isReset :: Seq Int -> Int -> Bool
+isReset (viewl -> EmptyL) _ = True
+isReset (viewl -> x:<xs) i
   | x /= i = False
   | otherwise = isReset xs (i + 1)
 
-split ::[Int] -> Hold -> Hold
-split r (Hold {q = q, row = b:bs}) = Hold (q ++ bs) (b:r)
+split :: Seq Int -> Hold -> Hold
+split r (Hold q (viewl -> b:<bs)) = Hold (q >< bs) (b <| r)
 
-tick :: Int -> [Int] -> [Int] -> Hold
-tick 0 q      newList   = Hold q newList
-tick i (x:xs) newList   = tick (i-1) xs (x : newList)
+tick :: Int -> Hold -> Hold
+tick 0 h = h
+tick i (Hold (viewl -> b:<bs) r) = tick (i-1) $ Hold bs (b <| r)
 
 tickFive :: Int -> Hold -> Hold
-tickFive 0 hold = hold
-tickFive i (Hold {q = q, row = r}) = tickFive (i - 1) $ split r $ tick 5 q []
+tickFive 0 h = h
+tickFive i (Hold q r) = tickFive (i - 1) $ split r $ tick 5 $ Hold q empty
 
 tickHour :: Int -> Hold -> Hold
 tickHour 0 hold = hold
-tickHour i (Hold {q = q, row = r}) = tickHour (i - 1) $ split r $ tickFive 12 $ Hold q []
+tickHour i (Hold q r) = tickHour (i - 1) $ split r $ tickFive 12 $ Hold q empty
 
-tickHalfDay :: [Int] -> [Int]
-tickHalfDay q = (\ (Hold {q = q, row = b:bs}) -> q ++ bs ++ [b]) $ tickHour 12 $ Hold q []
+tickHalfDay :: Seq Int -> Seq Int
+tickHalfDay q = (\ (Hold q (viewl -> b:<bs)) -> q >< bs >< singleton b) $ tickHour 12 $ Hold q empty
 
-go :: Int -> [Int] -> Int
+go :: Int -> Seq Int -> Int
 go i q
   | isReset q 1 && i /= 0 = i
   | otherwise = go (i + 1) (tickHalfDay (tickHalfDay q))
 
-main = print $ go 0 [1..30]
-
+main = print $ go 0 $ fromList [1..123]
